b31d584d4ec8e97ceb5f2bbc16b225d5255a6fe0
Andre Mendes <andre.eletrom@hotmail.com>
2023-05-15 17:35:49 -0300

    feat: added get products

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Product } from '../common/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  constructor(private http: HttpClient) { }
+
+  getProductList():Observable<Product[]> {
+    return this.http.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
 }
